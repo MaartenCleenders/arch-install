91778eefb3641f2023ac4c19d30d2d1abfc9ab65
maartencleenders <maarten.cleenders@live.be>
2020-09-15 23:47:37 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,9 +5,9 @@ rm -f arch.conf
 rm -f arch.sh
 rm -f arch-reboot.sh
 
-curl -O https://raw.githubusercontent.com/picodotdev/arch/master/arch.conf
-curl -O https://raw.githubusercontent.com/picodotdev/arch/master/arch.sh
-curl -O https://raw.githubusercontent.com/picodotdev/arch/master/arch-reboot.sh
+curl -O https://raw.githubusercontent.com/MaartenCleenders/arch-install/master/arch.conf
+curl -O https://raw.githubusercontent.com/MaartenCleenders/arch-install/master/arch.sh
+curl -O https://raw.githubusercontent.com/MaartenCleenders/arch-install/master/arch-reboot.sh
 
 chmod +x arch.sh
 chmod +x arch-reboot.sh
